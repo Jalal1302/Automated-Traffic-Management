61553493eabb1a7ef0fd671f6b59e72b5d0db095
Laurent Bialylew <laubia20@gmail.com>
2025-01-30 10:07:10 +0000

    classes

--- AutomatedTrafficManagement/system/models.py
+++ AutomatedTrafficManagement/system/models.py
@@ -7,6 +7,37 @@ class Vehicle(models.Model):
     owner_name = models.CharField (max_length= 100)
     vehicle_type = models.CharField(max_length= 10)
     registration_date = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def create(cls, number_plate, owner_name, vehicle_type, registration_date):
+        vehicle = cls(number_plate=number_plate, owner_name=owner_name, vehicle_type=vehicle_type, registration_date=registration_date)
+        vehicle.save
+        return vehicle
+
+class Road(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def create(cls, name):
+        road = cls(name=name)
+        road.save
+        return road
+
+
 class Junction(models.Model):
-    road = models.CharField(max_length= 20)
-    logged_vehicles = models.CharField(max_length= 100)
+    road = models.ManyToManyField(Road)
+    logged_vehicles = models.ManyToManyField(Vehicle)
+
+    def __str__(self):
+        return self.name
+    
+    def create(cls):
+        junction = cls()
+        junction.save
+        return junction
