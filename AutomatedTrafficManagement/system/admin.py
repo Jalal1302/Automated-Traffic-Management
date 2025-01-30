b322d74d9a71b9a2ce5ff533450a7116d0968078
Laurent Bialylew <laubia20@gmail.com>
2025-01-30 10:18:33 +0000

    updates

--- AutomatedTrafficManagement/system/admin.py
+++ AutomatedTrafficManagement/system/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Vehicle, Road, Junction
 # Register your models here.
+
+admin.site.register(Vehicle)
+admin.site.register(Road)
+admin.site.register(Junction)
