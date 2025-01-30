badd9cc880f9dc20f5f65a605fd5730f10d7b57d
Laurent-B2002 <laubia20@gmail.com>
2025-01-30 11:57:27 +0100

    Update models.py

--- AutomatedTrafficManagement/system/models.py
+++ AutomatedTrafficManagement/system/models.py
@@ -1,24 +1,28 @@
 from django.db import models
-
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 class Vehicle(models.Model):
-    number_plate = models.CharField(max_length= 10)
-    owner_name = models.CharField (max_length= 100)
-    vehicle_type = models.CharField(max_length= 10)
-    registration_date = models.DateTimeField(auto_now_add= True)
+    number_plate = models.CharField(max_length=10, unique=True)
+    owner_name = models.CharField(max_length=100)
+    vehicle_type = models.CharField(max_length=10)
+    registration_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.number_plate} - {self.owner_name}"
     
     @classmethod
-    def create(cls, number_plate, owner_name, vehicle_type, registration_date):
-        vehicle = cls(number_plate=number_plate, owner_name=owner_name, vehicle_type=vehicle_type, registration_date=registration_date)
-        vehicle.save
+    def create(cls, number_plate, owner_name, vehicle_type):
+        vehicle = cls(
+            number_plate=number_plate,
+            owner_name=owner_name,
+            vehicle_type=vehicle_type
+        )
+        vehicle.save()
         return vehicle
 
 class Road(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -26,18 +30,37 @@ class Road(models.Model):
     @classmethod
     def create(cls, name):
         road = cls(name=name)
-        road.save
+        road.save()
         return road
 
-
 class Junction(models.Model):
-    road = models.ManyToManyField(Road)
-    logged_vehicles = models.ManyToManyField(Vehicle)
+    name = models.CharField(max_length=100)
+    roads = models.ManyToManyField(Road)
+    logged_vehicles = models.ManyToManyField(Vehicle, blank=True)
 
     def __str__(self):
         return self.name
     
-    def create(cls):
-        junction = cls()
-        junction.save
+    def clean(self):
+        super().clean()
+        if self.id:  
+            if self.roads.count() < 2:
+                raise ValidationError(_('A junction must connect at least 2 roads.'))
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def create(cls, name, roads):
+        if len(roads) < 2:
+            raise ValidationError(_('A junction must be created with at least 2 roads.'))
+        
+        junction = cls(name=name)
+        junction.save()
+        junction.roads.set(roads)
         return junction
+    
+    def log_vehicle(self, vehicle):
+        self.logged_vehicles.add(vehicle)
+        
