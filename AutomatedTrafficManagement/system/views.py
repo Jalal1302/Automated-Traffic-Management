5250087bdf7ecd9c3ca51fdc4e9a9524102a19ec
Laurent Bialylew <laubia20@gmail.com>
2025-01-30 11:11:28 +0000

    forgot to add an import

--- AutomatedTrafficManagement/system/views.py
+++ AutomatedTrafficManagement/system/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 import json
 from .models import Vehicle, Road, Junction
 import requests
